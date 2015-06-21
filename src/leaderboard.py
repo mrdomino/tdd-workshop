cde29db4b65141ffed2de4397b6c61ca586b056d
Steven Dee <steven@choosemuse.com>
2015-06-21 16:23:32 -0400

    Tests pass again

--- src/leaderboard.py
+++ src/leaderboard.py
@@ -1,9 +1,9 @@
 class Leaderboard(object):
     def __init__(self):
-        self._x = 0
+        self._leaders = []
 
     def leaders(self):
-        return [object() for _ in range(self._x)]
+        return self._leaders
 
     def track_score(self, score, obj):
-        self._x = self._x + 1
+        self._leaders.append(obj)
