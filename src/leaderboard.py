04757519740326162b3f706b30e6662ab01d19e1
Steven Dee <steven@choosemuse.com>
2015-06-21 16:28:40 -0400

    Tests passing

--- src/leaderboard.py
+++ src/leaderboard.py
@@ -1,9 +1,11 @@
 class Leaderboard(object):
     def __init__(self, max=None):
         self._leaders = []
+        self._max = max
 
     def leaders(self):
         return self._leaders
 
     def track_score(self, score, obj):
-        self._leaders.append(obj)
+        if not self._max or len(self._leaders) < self._max:
+            self._leaders.append(obj)
