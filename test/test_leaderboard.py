c2a1c7362d3bff0dec95d6890d9201ba7910da5c
Steven Dee <steven@choosemuse.com>
2015-06-21 16:18:07 -0400

    Let's add another test
    First we make sure that it fails, so we know it's really testing
    something.

--- test/test_leaderboard.py
+++ test/test_leaderboard.py
@@ -5,3 +5,8 @@ class TestLeaderboard(unittest.TestCase):
     def test_empty(self):
         it = leaderboard.Leaderboard()
         self.assertEqual(0, len(it.leaders()))
+
+    def test_has_one_leader(self):
+        it = leaderboard.Leaderboard()
+        it.track_score(5, object())
+        self.assertEqual(1, len(it.leaders()))
