bbfa70c4b6a3a1f8b8d7b97771f98fe879a7a8c7
Steven Dee <steven@choosemuse.com>
2015-06-21 16:26:41 -0400

    New failing test

--- test/test_leaderboard.py
+++ test/test_leaderboard.py
@@ -16,3 +16,9 @@ class TestLeaderboard(unittest.TestCase):
         bob = object()
         it.track_score(1, bob)
         self.assertEqual(bob, it.leaders()[0])
+
+    def test_stores_up_to_max(self):
+        it = leaderboard.Leaderboard(max=5)
+        for i in range(5):
+            it.track_score(2, object())
+            self.assertEqual(i + 1, len(it.leaders()))
